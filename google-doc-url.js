4bc83b00e52e667a1860453d4974cf52eb76e01f
SilverKalule <162054797+SilverKalule@users.noreply.github.com>
2024-03-03 23:30:47 +0300

    Update google-doc-url.js
    Updated Url

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1ZxvU8eGyuN9M8GxTU9acKVJv70iC3px_m3EVFsOHN9g/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/18gr0nw4U4wxMm_ClO3lJdgID4aHXns1Mj90aE3XUZeU/edit#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyBh9nKnVZm2RPeZa0ywCOxPAgJJfK87WhY';
